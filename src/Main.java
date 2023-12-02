9db82851b8d9199b7a7d35a55aa083a237fa5b55
Michal Wabnik <michal-w@hotmail.com>
2023-12-02 20:26:34 +0100

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import com.isa.javabase.Adres;
+import com.isa.javabase.Person;
+import com.isa.javabase.StaticExamples;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.print("Text 1");
+        System.out.println("Text 2");
+
+
+        Adres adres1 = new Adres("Warszawa", "Mazowiecka",12);
+        System.out.println("Miasto: " + adres1.getCity() + ", ulica: " + adres1.getStreet() + " " + adres1.getNumber());
+        Person person1 = new Person("Michal", 38, adres1);
+        System.out.println(person1.getNameAndAgeAndCity() + ", ulica: " + person1.getAdres().getCity() + " " + person1.getAdres().getNumber());
+
+        StaticExamples static1 = new StaticExamples();
+        System.out.println(StaticExamples.staticMethod());
+        static1.instanceMethod();
+    }
+}
