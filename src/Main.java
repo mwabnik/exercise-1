67bd1cdd1b5d2f398e97ce95ea5113b11b0aaeab
Michal Wabnik <michal-w@hotmail.com>
2023-12-03 19:50:33 +0100

    Add Table

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         System.out.print("Text 1");
         System.out.println("Text 2");
 
-
         Adres adres1 = new Adres("Warszawa", "Mazowiecka",12);
         System.out.println("Miasto: " + adres1.getCity() + ", ulica: " + adres1.getStreet() + " " + adres1.getNumber());
         Person person1 = new Person("Michal", 38, adres1);
---
+++ src/Table.java
@@ -0,0 +1,13 @@
+public class Table {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+        nums[0] = 2;
+        nums[1] = 4;
+        int[] nums2 = {1, 2, 3, 4, 5};
+
+        System.out.println(nums[1]);
+        System.out.println(nums2[2]);
+        nums[1] = 4;
+        System.out.println(nums[1]);
+    }
+}
