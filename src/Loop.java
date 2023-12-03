e7427e8e4acf9cfef8306086554c3269cb177d4b
Michal Wabnik <michal-w@hotmail.com>
2023-12-03 20:44:44 +0100

    Add for loops

--- src/Loop.java
+++ src/Loop.java
@@ -1,9 +1,14 @@
 public class Loop {
     public static void main(String[] args) {
         Loop loop = new Loop();
-        loop.ex1();
-        System.out.println();
-        loop.ex2();
+//        System.out.println("ex1");
+//        loop.ex1();
+//        System.out.println("=======");
+//        System.out.println("ex2");
+//        loop.ex2();
+//        System.out.println("Imona to: ");
+//        loop.ex3();
+
     }
     public void ex1() {
         for( int i = 1; i < 15; i++){
@@ -16,4 +21,11 @@ public class Loop {
             System.out.println(i);
         }
     }
+    public void ex3() {
+        String[] names = {"Michal", "Adam", "Jurek"};
+        for (String name: names) {
+            System.out.println(name);
+        }
+    }
+
 }
