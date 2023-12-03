836a8c52f8f03ada6a62eae4cf55c9017c0ee29c
Michal Wabnik <michal-w@hotmail.com>
2023-12-03 20:53:35 +0100

    Add while loops

--- src/Loop.java
+++ src/Loop.java
@@ -8,24 +8,39 @@ public class Loop {
 //        loop.ex2();
 //        System.out.println("Imona to: ");
 //        loop.ex3();
+//        loop.ex4();
+        loop.doWhileLoop();
 
     }
-    public void ex1() {
+    public void forLoop1() {
         for( int i = 1; i < 15; i++){
             System.out.println(i);
         }
     }
-    public void ex2() {
+    public void forLoop2() {
         for( int i = 1; i < 15;){
             i += 2;
             System.out.println(i);
         }
     }
-    public void ex3() {
+    public void forShortLoop() {
         String[] names = {"Michal", "Adam", "Jurek"};
         for (String name: names) {
             System.out.println(name);
         }
     }
-
+    public void whileLoop() {
+        int i = 1;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+    public void doWhileLoop() {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+    }
 }
